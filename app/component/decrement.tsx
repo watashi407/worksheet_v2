702374585ddef46b95be25099b2273aeeccadde2
watashi407 <hyperteddy70@gmail.com>
2025-01-13 17:10:07 +0800

    new update

---
+++ app/component/decrement.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function decrement() {
+  return <div>decrement</div>;
+}
+
+export default decrement;
---
+++ app/component/reset.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const reset = () => {
+  return <div>reset</div>;
+};
+
+export default reset;
